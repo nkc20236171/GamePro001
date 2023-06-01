01cdf5299504dc2fa7959eabd083f14096f9b776
nkc_suga <nkc23.suga.ryosei@gmail.com>
2023-06-01 11:11:38 +0900

    601

--- Assets/MyCharControl.cs
+++ Assets/MyCharControl.cs
@@ -7,33 +7,43 @@ public class MyCharControl : MonoBehaviour
     [SerializeField] float speed;
     float xLimit = 8.5f;
     float yLimit = 4.5f;
-
+    Animator anim;
     void Start()
     {
         Application.targetFrameRate = 60;
+        anim = GetComponent<Animator>();
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
         float spped = Time.time;
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        float x = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3( x * 0.14f,0);
+
+        float y = Input.GetAxisRaw("Vertical");
+        transform.position += new Vector3(0,y * 0.14f);
+        if (y == 0)
         {
-            transform.Translate(-0.1f, 0, 0);
+            anim.Play("player");
+           
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        else if (y == 1)
         {
-            transform.Translate(0.1f, 0, 0);
+            anim.Play("PlayerL");
+            
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if(y ==-1)
         {
-            transform.Translate(0, -0.1f, 0);
+            anim.Play("playerR");
+            
+
+
         }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            transform.Translate(0, 0.1f, 0);
-        }
+
         Vector3 currentPos = transform.position;
 
         currentPos.x = Mathf.Clamp(currentPos.x, -xLimit, xLimit);
